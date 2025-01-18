e6d337bcdac6f251d83b32c03e075d20ee3e6ef1
Ctfy21 <skileev@bk.ru>
2025-01-18 19:17:23 +0300

    add auto update ip

--- cbt_timelapses_frontend/src/socket.js
+++ cbt_timelapses_frontend/src/socket.js
@@ -1,6 +1,5 @@
-
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-    process.env.NODE_ENV === "production" ? "ws://192.168.42.119:5000/ws" : "ws://192.168.42.119:5000/ws";
+    process.env.NODE_ENV === "production" ? "ws://"+process.env.VUE_APP_PATH_START+":5000/ws" : "ws://"+process.env.VUE_APP_PATH_START+":5000/ws";
 
 export const socket = new WebSocket(URL);
--- startserver.sh
+++ startserver.sh
@@ -2,10 +2,14 @@
 
 export LANG=""; 
 
-IP=$(hostname -I)
+SAMBA_IP="//192.168.1.1/snapshots"
+HOST_IP=$(hostname -I)
 
-touch .env 
-echo "LOCAL_IP=${IP}" >> .env
+watch -n 86400 mount.cifs $SAMBA_IP /mnt/seefetch -o user=sambauser,pass=111111111
+
+touch /home/blunder/bin/cbt_timelapses_new/cbt_timelapses_frontend/.env 
+echo "VUE_APP_PATH_START=${HOST_IP}" >> /home/blunder/bin/cbt_timelapses_new/cbt_timelapses_frontend/.env
+
 redis-server &&
 serve -p 8080 -s /home/blunder/bin/cbt_timelapses_new/cbt_timelapses_frontend/dist/ &&
 /home/blunder/bin/cbt_timelapses_new/cbt_timelapses_backend/main

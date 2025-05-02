96631e3350cb94956f73c935adaac95782784632
Ctfy21 <70262432+Ctfy21@users.noreply.github.com>
2025-05-02 16:15:16 +0300

    Update startserver.sh

--- startserver.sh
+++ startserver.sh
@@ -10,6 +10,5 @@ watch -n 86400 mount.cifs $SAMBA_IP /mnt/seefetch -o user=sambauser,pass=1111111
 touch /home/cbt_timelapses_new/cbt_timelapses_frontend/.env 
 echo "VUE_APP_PATH_START=${HOST_IP}" >> /home/cbt_timelapses_new/cbt_timelapses_frontend/.env
 
-redis-server &&
 serve -p 8080 -s /home/cbt_timelapses_new/cbt_timelapses_frontend/dist/ &&
 /home/cbt_timelapses_new/cbt_timelapses_backend/main

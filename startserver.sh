7a907fe55192e70e96ee85c353a6a8acd67f9479
Ctfy21 <70262432+Ctfy21@users.noreply.github.com>
2025-05-02 16:46:47 +0300

    Update startserver.sh

--- startserver.sh
+++ startserver.sh
@@ -7,8 +7,8 @@ HOST_IP=$(hostname -I)
 
 watch -n 86400 mount.cifs $SAMBA_IP /mnt/seefetch -o user=sambauser,pass=111111111
 
-touch /home/cbt_timelapses_new/cbt_timelapses_frontend/.env 
-echo "VUE_APP_PATH_START=${HOST_IP}" >> /home/cbt_timelapses_new/cbt_timelapses_frontend/.env
+touch /home/blunder/bin/cbt_timelapses_new/cbt_timelapses_frontend/.env 
+echo "VUE_APP_PATH_START=${HOST_IP}" >> /home/blunder/bin/cbt_timelapses_new/cbt_timelapses_frontend/.env
 
-serve -p 8080 -s /home/cbt_timelapses_new/cbt_timelapses_frontend/dist/ &&
-/home/cbt_timelapses_new/cbt_timelapses_backend/main
+serve -p 8080 -s /home/blunder/bin/cbt_timelapses_new/cbt_timelapses_frontend/dist/ &
+/home/blunder/bin/cbt_timelapses_new/cbt_timelapses_backend/main

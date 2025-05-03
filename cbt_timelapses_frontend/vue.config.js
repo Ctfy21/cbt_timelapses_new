b652aab4d92b3d8fea77a2d6049ca53fac4316e5
Ctfy21 <70262432+Ctfy21@users.noreply.github.com>
2025-05-03 22:55:15 +0300

    Update vue.config.js

--- cbt_timelapses_frontend/vue.config.js
+++ cbt_timelapses_frontend/vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
 module.exports = defineConfig({
   transpileDependencies: true,
+  devServer: {
+    port: 8080,
+  }
   configureWebpack: {
     plugins: [new NodePolyfillPlugin()],
     optimization: {

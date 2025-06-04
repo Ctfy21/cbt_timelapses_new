993cf2f24273eaac4c5e01976e7da8f0fcc8e287
Ctfy21 <70262432+Ctfy21@users.noreply.github.com>
2025-06-04 14:52:25 +0300

    Update server.js

--- cbt_timelapses_frontend/server.js
+++ cbt_timelapses_frontend/server.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 8080;
-var hostname = process.env.VUE_APP_PATH_START;
+// var express = require('express');
+// var path = require('path');
+// var serveStatic = require('serve-static');
+// app = express();
+// app.use(serveStatic(__dirname + "/dist"));
+// var port = process.env.PORT || 8080;
+// var hostname = process.env.VUE_APP_PATH_START;
 
-app.listen(port, hostname, () => {
-   console.log(`Server running at http://${hostname}:${port}/`);
- });
+// app.listen(port, hostname, () => {
+//    console.log(`Server running at http://${hostname}:${port}/`);
+//  });
